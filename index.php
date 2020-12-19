1d0696d9358b9ab8bb7a1e2792a337b7f29dde82
MattCod3s <Kostka>
2020-12-19 11:41:53 +0000

    Some work done

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+
+<form action="index.php" method="post">
+    <label>Name : </label>
+    <input type="text" name="name"/>
+</form>
+
 <?php 
- echo "hello";
+    $name = $_POST["name"];
+    echo $name;
 ?>
